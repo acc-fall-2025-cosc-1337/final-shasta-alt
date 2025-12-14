0357198e59589043db3030f810db15060eef745e
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 19:23:48 -0600

    Implement Roll class for dice rolling functionality

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,15 @@
-//
+#include "Roll.h"
+
+Roll::Roll(Die& d1, Die& d2) : die1(d1), die2(d2), rolled_value(0)
+{
+}
+
+void Roll::roll_dice()
+{
+    rolled_value = die1.roll() + die2.roll();
+}
+
+int Roll::roll_value() const
+{
+    return rolled_value;
+}
