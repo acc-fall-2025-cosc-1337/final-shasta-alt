b8f1752026d9d97b0528b27f2efba97fcf347bdb
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 19:25:25 -0600

    Implement main function for dice rolling
    Added main function to roll dice and validate results.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <ctime>
+
+#include "Die.h"
 #include "Roll.h"
 
-Roll::Roll(Die& d1, Die& d2) : die1(d1), die2(d2), rolled_value(0)
+int main()
 {
+    srand(time(0));
+
+    Die die1;
+    Die die2;
+    Roll roll(die1, die2);
+
+    for(int i = 0; i < 10; i++)
+    {
+        roll.roll_dice();
+        int value = roll.roll_value();
+        std::cout << value << std::endl;
+        assert(value >= 2 && value <= 12);
+    }
+
+    std::cout << "Question 2 test passed" << std::endl;
+    return 0;
 }
 
-void Roll::roll_dice()
-{
-    rolled_value = die1.roll() + die2.roll();
-}
-
-int Roll::roll_value() const
-{
-    return rolled_value;
-}
 
