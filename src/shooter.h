b66f55b9c34d334ba0ba74a131444b7121d87b08
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 20:31:41 -0600

    Add Shooter class header with dice throwing methods

--- src/shooter.h
+++ src/shooter.h
@@ -1,1 +1,18 @@
-//
+#ifndef SHOOTER_H
+#define SHOOTER_H
+
+#include <vector>
+#include "Roll.h"
+
+class Shooter
+{
+public:
+    Roll* throw_dice(Die& d1, Die& d2);
+    void display_rolled_values();
+    ~Shooter();
+
+private:
+    std::vector<Roll*> rolls;
+};
+
+#endif
