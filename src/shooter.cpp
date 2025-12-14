15c5b8008f7c75f6d3eb769d3d57144aa9dc6d54
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 20:31:58 -0600

    Implement dice throwing and value display in Shooter

--- src/shooter.cpp
+++ src/shooter.cpp
@@ -1,1 +1,26 @@
-//
+#include "Shooter.h"
+#include <iostream>
+
+Roll* Shooter::throw_dice(Die& d1, Die& d2)
+{
+    Roll* roll = new Roll(d1, d2);
+    roll->roll_dice();
+    rolls.push_back(roll);
+    return roll;
+}
+
+void Shooter::display_rolled_values()
+{
+    for(auto r : rolls)
+    {
+        std::cout << r->roll_value() << std::endl;
+    }
+}
+
+Shooter::~Shooter()
+{
+    for(auto r : rolls)
+    {
+        delete r;
+    }
+}
